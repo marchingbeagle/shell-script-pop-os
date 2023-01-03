e119da2fa9814b8902e7aa4d420910e60f5759e6
erik <erik.schneider.pachco@gmail.com>
2023-01-03 01:22:12 -0300

    Gparted added and code better organized

--- script.sh
+++ script.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+# variables
 DIRETORIO_DOWNLOADS="$HOME/Downloads/"
 URL_GOOGLE_CHROME="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
 
+# nala install and update for the system
 sudo apt install nala -y
 sudo nala update -y
 sudo nala upgrade -y
 
+# flatpak installs
 sudo nala install flatpak -y;
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-
 flatpak install flathub com.discordapp.Discord -y;
 flatpak install flathub com.spotify.Client -y;
 flatpak install flathub com.bitwarden.desktop -y;
@@ -19,15 +21,16 @@ flatpak install flathub com.visualstudio.code -y;
 flatpak install flathub net.lutris.Lutris -y;
 flatpak update -y;
 
+# snap installs
 sudo nala install snapd -y;
 sudo snap install notion-snap -y;
 
+# standard nala installs
 sudo nala install gnome-tweaks -y;
-
 sudo nala install ufw -y;
 sudo nala install gufw -y;
+sudo nala install gparted -y;
 
 # Download and install chrome deb
 wget -c "$URL_GOOGLE_CHROME"       -P "$DIRETORIO_DOWNLOADS"
 sudo dpkg -i $DIRETORIO_DOWNLOADS/*.deb
-
