7c873ffa6ec3236dcb7b5428619faf5d5c43e7bc
erik <erik.schneider.pachco@gmail.com>
2023-01-04 00:14:52 -0300

    addition of ufw configuration

--- script.sh
+++ script.sh
@@ -32,6 +32,14 @@ sudo nala install gufw -y;
 sudo nala install gparted -y;
 sudo nala install gimp -y;
 
+# ufw configuration
+sudo ufw limit 22/tcp
+sudo ufw allow 80/tcp
+sudo ufw allow 443/tcp
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+sudo ufw enable
+
 # Download and install chrome deb
 wget -c "$URL_GOOGLE_CHROME"       -P "$DIRETORIO_DOWNLOADS"
 sudo dpkg -i $DIRETORIO_DOWNLOADS/*.deb
