027b8709b5675139be050da704a320d865dc7845
Erik Schneider <66921966+marchingbeagle@users.noreply.github.com>
2023-02-07 12:05:22 -0300

    nvm added

--- script.sh
+++ script.sh
@@ -47,6 +47,10 @@ sudo npm cache clean -f;
 sudo npm install -g n;
 sudo n stable;
 hash -r
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+cd
+echo "export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" " >> /.zshrc
 
 # ufw configuration
 sudo ufw limit 22/tcp;
